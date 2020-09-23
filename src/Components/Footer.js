b68270ff3cc91ba3a576ceda972313b877317d6b
Wade Butler <wadebutler2@gmail.com>
2020-09-23 12:51:55 -0400

    built header and footer component, added base styles

---
+++ src/Components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <ul>
+                <li>About</li>
+                <li>PLB Bonus</li>
+                <li>Contact</li>
+                <li>Security</li>
+                <li>Responsible Play</li>
+                <li>Privacy</li>
+                <li>Terms</li>
+            </ul>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>
+                <img src={require("../assets/logo/logo@1x.png")} alt="playline sports lotto logo" />
+            </h1>
+        </header>
+    );
+}
+
+export default Header;
